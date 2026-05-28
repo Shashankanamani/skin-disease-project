ed582712ae96aae2149b250e0bf654d06b3b3b19
Shashankanamani <146080434+Shashankanamani@users.noreply.github.com>
2026-05-29 02:38:29 +0530

    Updated app for Jenkins demo

--- app.py
+++ app.py
@@ -1,70 +1,10 @@
-from flask import Flask, request, render_template_string
-import tensorflow as tf
-import numpy as np
-from PIL import Image
+from flask import Flask
 
 app = Flask(__name__)
 
-# LOAD MODEL
-model = tf.keras.models.load_model("model.h5")
+@app.route('/')
+def home():
+    return "Skin Disease Detection App Running Successfully!"
 
-# CLASS NAMES
-classes = [
-    "atopic",
-    "basal_cell_carcinoma",
-    "melanoma",
-    "psoriasis"
-]
-
-HTML = """
-
-<h2>Skin Disease Detection</h2>
-
-<form method="POST" enctype="multipart/form-data">
-
-    <input type="file" name="file">
-
-    <input type="submit">
-
-</form>
-
-{% if prediction %}
-
-<h3>Prediction: {{ prediction }}</h3>
-
-{% endif %}
-
-"""
-
-@app.route("/", methods=["GET", "POST"])
-
-def predict():
-
-    prediction = ""
-
-    if request.method == "POST":
-
-        file = request.files["file"]
-
-        image = Image.open(file)
-
-        image = image.resize((128,128))
-
-        image = np.array(image) / 255.0
-
-        image = np.expand_dims(image, axis=0)
-
-        pred = model.predict(image)
-
-        predicted_class = np.argmax(pred)
-
-        prediction = classes[predicted_class]
-
-    return render_template_string(
-        HTML,
-        prediction=prediction
-    )
-
-if __name__ == "__main__":
-
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
